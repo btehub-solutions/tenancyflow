400b46b0d407177ac9ff761fe0ed9d555532c1b8
btehub-solutions <btehubsolutions@gmail.com>
2026-03-13 23:29:54 -0800

    Fix: Remove hanging migrate command from build script

--- build.sh
+++ build.sh
@@ -2,7 +2,13 @@
 # exit on error
 set -o errexit
 
-pip install -r requirements.txt --break-system-packages
+# pip install -r requirements.txt is handled automatically by Vercel
+# during the build process for the @vercel/python builder.
 
+echo "Running collectstatic..."
 python manage.py collectstatic --no-input
-python manage.py migrate
+
+# Migrations should not be run during the build phase on Vercel 
+# as it can hang due to network restrictions or missing env vars.
+# python manage.py migrate
+
